8e117c5ebd51fd900ca1a55f9062a5d053b70b31
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-17 16:35:54 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,6 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={'min-h-screen flex flex-col items-center justify-center '}>
-      <Card className={'w-[350px]'}>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-        </CardHeader>
-        <CardContent className={'flex flex-col gap-2'}>
-          <Button>
-            <Link href={'/signin'}>Sign In</Link>
-          </Button>
-          <Button>
-            <Link href={'/signup'}>Sign Up</Link>
-          </Button>
-        </CardContent>
-      </Card>
-    </main>
+
   );
 }
